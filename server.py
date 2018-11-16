648873cf8056a6379e91acfd2f4f52b329170b3a
Lydia Nishimwe <nishimwelydia@yahoo.com>
2018-11-16 22:15:59 +0100

    add url to pubmed article

--- server.py
+++ server.py
@@ -1,12 +1,32 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from scholarly import search_author
-from googlesearch import search
-
+import requests
+import credentials
 
 app = Flask(__name__)
 api = Api(app)
 
+CUSTOM_SEARCH_API_URL = 'https://www.googleapis.com/customsearch/v1/siterestrict'
+
+class CustomSearchError(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+def searchArticleURL(title):
+    payload = {'key': credentials.GOOGLE_SEARCH_API_KEY,
+                'cx': credentials.CUSTOM_SEARCH_ENGINE_ID,
+                'q': title}
+    response = requests.get(CUSTOM_SEARCH_API_URL, params=payload)
+    data = dict(response.json())
+    if 'error' in data:
+        raise CustomSearchError(data['error']['code'], data['error']['message'])
+    totalResults = int(data['searchInformation']['totalResults'])
+    if totalResults > 0:
+        return data['items'][0]['link']
+    else:
+        return ''
 
 class Author(Resource):
     def get(self):
@@ -14,11 +34,16 @@ class Author(Resource):
             author_name = request.args.get('name')
             search_query = search_author(author_name)
             author = next(search_query).fill()
-            articles = [{'title': pub.bib['title'],
-                        'year': pub.bib['year'] if 'year' in pub.bib else '',
-                        'citationCount': pub.citedby if hasattr(pub, 'citedby') else '',
-                        'url': next(search('pmid '+ pub.bib['title'], num=1,tld="co.in",stop=0))}
-                        for pub in author.publications]
+            articles = []
+            for pub in author.publications:
+                title = pub.bib['title']
+                articleURL = searchArticleURL(title)
+                if articleURL:
+                    article = {'title': title,
+                            'year': pub.bib['year'] if 'year' in pub.bib else '',
+                            'citationCount': pub.citedby if hasattr(pub, 'citedby') else '',
+                            'url': articleURL}
+                    articles.append(article)
             result = {'name': author.name,
                     'hIndex': author.hindex,
                     'citationCount': author.citedby,
@@ -27,6 +52,8 @@ class Author(Resource):
             return result, 200, {'Access-Control-Allow-Origin': '*'}
         except StopIteration:
             return {'error': 'No Google Scholar profile found'}, 200, {'Access-Control-Allow-Origin': '*'}
+        except CustomSearchError as err:
+             return {'error': err.message }, err.code, {'Access-Control-Allow-Origin': '*'}
 
 api.add_resource(Author, '/author')
 
--- test.py
+++ test.py
@@ -3,22 +3,35 @@ import time
 from googlesearch import search
 import requests
 
+
+class CustomSearchError(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+
 start_time = time.time()
 payload = {'key':'AIzaSyBcRhfxFn9Y3S-G63p-kApyTE9BhUmeBTA',
             'cx': '005251479122416850940:oiwjxnvfi00',
             'q': 'NPHS2 V260E Is a Frequent Cause of Steroid-Resistant Nephrotic Syndrome in Black South African Children'}
-response = requests.get('https://www.googleapis.com/customsearch/v1', params=payload)
+response = requests.get('https://www.googleapis.com/customsearch/v1/siterestrict', params=payload)
 data = dict(response.json())
+print(data)
+if 'error' in data:
+    raise CustomSearchError(data['error']['code'], data['error']['message'])
 total = int(data['searchInformation']['totalResults'])
+print(total)
 if total > 0:
     article = data['items'][0]['link']
     print(article)
 print((time.time() - start_time), 'seconds')
 
+
 """ start_time = time.time()
 url = next(search('pmid Multicohort genomewide association study reveals a new signal of protection against HIV-1 acquisition', num=1,stop=0))
 print(url)
 print((time.time() - start_time), 'seconds') """
+
 """start_time = time.time()
 search_query = scholarly.search_author('sophie limou')
 author = next(search_query).fill()
