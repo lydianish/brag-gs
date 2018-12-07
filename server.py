d1333b8ceda5f00e115e1e2143c3cc50b55dc0b7
Lydia Nishimwe <nishimwelydia@yahoo.com>
2018-12-07 17:33:23 +0100

    no use of google custom search

--- server.py
+++ server.py
@@ -2,31 +2,9 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from scholarly import search_author
 import requests
-import credentials
 
 app = Flask(__name__)
 api = Api(app)
-
-CUSTOM_SEARCH_API_URL = 'https://www.googleapis.com/customsearch/v1/siterestrict'
-
-class CustomSearchError(Exception):
-    def __init__(self, code, message):
-        self.code = code
-        self.message = message
-
-def searchArticleURL(title):
-    payload = {'key': credentials.GOOGLE_SEARCH_API_KEY,
-                'cx': credentials.CUSTOM_SEARCH_ENGINE_ID,
-                'q': title}
-    response = requests.get(CUSTOM_SEARCH_API_URL, params=payload)
-    data = dict(response.json())
-    if 'error' in data:
-        raise CustomSearchError(data['error']['code'], data['error']['message'])
-    totalResults = int(data['searchInformation']['totalResults'])
-    if totalResults > 0:
-        return data['items'][0]['link']
-    else:
-        return ''
 
 class Author(Resource):
     def get(self):
@@ -49,20 +27,8 @@ class Author(Resource):
             return result, 200, {'Access-Control-Allow-Origin': '*'}
         except StopIteration:
             return {'error': 'No Google Scholar profile found'}, 200, {'Access-Control-Allow-Origin': '*'}
-        except CustomSearchError as err:
-             return {'error': err.message }, err.code, {'Access-Control-Allow-Origin': '*'}
 
 api.add_resource(Author, '/author')
 
 if __name__ == '__main__':
     app.run(port='5002')
-
-""" for pub in author.publications:
-                title = pub.bib['title']
-                articleURL = searchArticleURL(title)
-                if articleURL:
-                    article = {'title': title,
-                            'year': pub.bib['year'] if 'year' in pub.bib else '',
-                            'citationCount': pub.citedby if hasattr(pub, 'citedby') else '',
-                            'url': articleURL}
-                    articles.append(article) """
